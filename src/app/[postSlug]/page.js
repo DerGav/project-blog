845323dac1361fda06e6eff2726c7a8e411ac6bb
gavingabriel <gavin.gabriel@susiandjames.com>
2025-12-08 17:49:42 +0100

    exercise 6: circular colors demo

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -12,6 +12,9 @@ import styles from "./postSlug.module.css";
 const DivisionGroupsDemo = dynamic(() =>
   import("@/components/DivisionGroupsDemo")
 );
+const CircularColorsDemo = dynamic(() =>
+  import("@/components/CircularColorsDemo")
+);
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
@@ -39,7 +42,11 @@ async function BlogPost({ params }) {
       <div className={styles.page}>
         <MDXRemote
           source={content}
-          components={{ pre: CodeSnippet, DivisionGroupsDemo }}
+          components={{
+            pre: CodeSnippet,
+            DivisionGroupsDemo,
+            CircularColorsDemo,
+          }}
         />
       </div>
     </article>
